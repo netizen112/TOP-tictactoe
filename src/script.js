288d6c3dfb3a93095faad01e917556e37413d789
netizen112 <netizen112@gmail.com>
2023-09-18 17:31:39 +0200

    make basic game functionality playable via console

--- src/script.js
+++ src/script.js
@@ -5,19 +5,10 @@ let gameBoardModule = (function () {
         if (gameArray.length > 0) {
             gameArray.splice(0);
         }
-        // gameArray[0] = "0";
-        // gameArray[1] = "1";
-        // gameArray[2] = "2";
-        // gameArray[3] = "3";
-        // gameArray[4] = "4";
-        // gameArray[5] = "5";
-        // gameArray[6] = "6";
-        // gameArray[7] = "7";
-        // gameArray[8] = "8";
 
-        gameArray[0] = "X";
-        gameArray[1] = "X";
-        gameArray[2] = "X";
+        gameArray[0] = "0";
+        gameArray[1] = "1";
+        gameArray[2] = "2";
         gameArray[3] = "3";
         gameArray[4] = "4";
         gameArray[5] = "5";
@@ -30,8 +21,14 @@ let gameBoardModule = (function () {
         return gameArray;
     }
 
-    return { initializeArray, getGameArray };
+    function setGameArray(index, letter) {
+        gameArray[index] = letter;
+    }
+
+    return { initializeArray, getGameArray, setGameArray };
 })();
+
+// ----------------------------------------------------------------- //
 
 const playerFactory = (name, letter) => {
     let gamesPlayed = 0;
@@ -40,35 +37,52 @@ const playerFactory = (name, letter) => {
     return { name, gamesPlayed, wins, losses, letter };
 };
 
+// ----------------------------------------------------------------- //
+
 let gameModule = (function () {
     let player1, player2;
 
     let round = {
         num: 0,
         currPlayer: 0,
-        game: 0
+        game: 0,
     };
 
     let currPos = {
         tile: 9,
-        letter: 'Z'
-    }
+        player: 0,
+    };
 
     //sets up a new game
     function newGame(p1name, p1letter, p2name, p2letter) {
         player1 = playerFactory(p1name, p1letter);
         player2 = playerFactory(p2name, p2letter);
 
-        round.num = 1;
-        round.currPlayer = 1;
+        round.num = 0;
+
+        return "New Game Successfully Created";
     }
 
-    function playRound(currPos) {
-        if (round.num == 0){
+    function playRound(tile, player) {
+        if (round.num == 0) {
             round.game++;
             round.currPlayer = 1;
         }
 
+        currPos.tile = tile;
+        currPos.player = player;
+
+        let currLetter =
+            currPos.player == 1
+                ? player1.letter
+                : currPos.player == 2
+                ? player2.letter
+                : "ERROR";
+        gameBoardModule.setGameArray(currPos.tile, currLetter);
+        checkWin(currPos)
+
+        round.currPlayer = round.currPlayer == 1 ? 2 : 1;
+        round.num++;
     }
 
     //Checks if there is a win. Parameter - Current Position (Object?)
@@ -118,33 +132,42 @@ let gameModule = (function () {
     }
 
     function winGame(currPos) {
-        console.log("win");
+        console.log(`Player `);
+
+
     }
 
     function checkGame(currPos) {}
 
-    return { checkWin, newGame };
+    function getPlayer(num) {
+        return num == 1 ? player1 : num == 2 ? player2 : -1;
+    }
+
+    return { checkWin, newGame, playRound, getPlayer };
 })();
 
-const displayControlModule = (function () {
+// ----------------------------------------------------------------- //
 
-    function handleClick(e){
+const displayControlModule = (function () {
+    function handleClick(e) {
         console.log(e);
     }
 
-    function initializeGame(){
-        let tiles = document.querySelectorAll('.tile');
+    function initializeGame() {
+        let btnNewGame = document.querySelector("#new-game");
+        btnNewGame.addEventListener("click", () => {});
+
+        let tiles = document.querySelectorAll(".tile");
         tiles.forEach((tile) => {
-            tile.addEventListener('click', handleClick);
-        })
+            tile.addEventListener("click", handleClick);
+        });
     }
 
-    return ({initializeGame});
+    return { initializeGame };
 })();
 
-console.log(gameBoardModule.getGameArray());
+// ----------------------------------------------------------------- //
+
 gameBoardModule.initializeArray();
-console.log(gameBoardModule.getGameArray());
-gameModule.checkWin(5);
 
 displayControlModule.initializeGame();
